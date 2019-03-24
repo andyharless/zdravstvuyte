54c557032b636315a87d300c5162fed1d3fbc565
andy <andy@andy-VirtualBox>
2019-03-23 21:28:07 -0400

    add Spanish and Australian

--- zdravstvuyte/src/BuonGiorno.scala
+++ zdravstvuyte/src/BuonGiorno.scala
@@ -5,10 +5,10 @@ object BuonGiorno extends App {
   val lang:String = "Italian"
   val greeting:String = {
     if (lang=="French") "Bon jour"
-    else {
-      if (lang=="Italian") "Buon giorno"
+    else if (lang=="Italian") "Buon giorno"
+    else if (lang=="Spanish") "Buenos dias"
+    else if (lang=="Australian") "G'day"
       else "Hello"
-    }
   }
   println(greeting + ", " + who + "!")
 }
