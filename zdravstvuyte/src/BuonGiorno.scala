2786dabcd01080320db8af99c6dc2872726ba51d
andy <andy@andy-VirtualBox>
2019-03-24 13:59:03 -0400

    replace pattern match with maps

--- zdravstvuyte/src/BuonGiorno.scala
+++ zdravstvuyte/src/BuonGiorno.scala
@@ -1,15 +1,20 @@
 
 
 object BuonGiorno extends App {
-  val lang:String = "Spanish"
-  val values:(String,String) = lang match {
-    case "French" => ("Bon jour", "Monde")
-    case "Italian" => ("Buon giorno", "Mondo")
-    case "Spanish" => ("Buenos dias", "Mundo")
-    case "Australian" => ("G'day", "World")
-    case "Cat" => ("Miao", "Purr")
-    case _ => ("Hello", "World")
-  }
-  val (greeting, who) = values
+  val greetings: Map[String, String] = Map(
+                      "French"->"Bon jour",
+                      "Italian"->"Buon giorno",
+                      "Spanish"->"Buenos dias",
+                      "Australian"->"G'day",
+                      "Cat"->"Miao")
+  val recipient: Map[String, String] = Map(
+                      "French"->"Monde",
+                      "Italian"->"Mondo",
+                      "Spanish"->"Mundo",
+                      "Australian"->"World",
+                      "Cat"->"Purr")
+  val lang:String = "French"
+  val greeting: String = greetings getOrElse (lang, "Hello")
+  val who: String = recipient getOrElse(lang, "World")
   println(greeting + ", " + who + "!")
 }
