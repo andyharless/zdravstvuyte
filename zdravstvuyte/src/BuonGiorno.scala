d838c54e2c5a002f659ac328b3b505eb402ea720
andy <andy@andy-VirtualBox>
2019-03-23 21:50:00 -0400

    change if/else to match

--- zdravstvuyte/src/BuonGiorno.scala
+++ zdravstvuyte/src/BuonGiorno.scala
@@ -3,13 +3,13 @@
 object BuonGiorno extends App {
   val who:String = "Mondo"
   val lang:String = "Italian"
-  val greeting:String = {
-    if (lang=="French") "Bon jour"
-    else if (lang=="Italian") "Buon giorno"
-    else if (lang=="Spanish") "Buenos dias"
-    else if (lang=="Australian") "G'day"
-    else if (lang=="Cat") "Miao"
-      else "Hello"
+  val greeting:String = lang match {
+    case "French" => "Bon jour"
+    case "Italian" => "Buon giorno"
+    case "Spanish" => "Buenos dias"
+    case "Australian" => "G'day"
+    case "Cat" => "Miao"
+    case _ => "Hello"
   }
   println(greeting + ", " + who + "!")
 }
